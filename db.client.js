a8abce3405608c277d69914a7a85d3205c915ff0
ItchirMaissa <itchirmaissa@gmail.com>
2025-02-23 12:34:00 +0100

    second commit

--- db.client.js
+++ db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  process.env.VARIABLE,
+  process.env.DATABASE_URL,
   'postgres://fakeurl', // TODO
   {
     dialectOptions: {
